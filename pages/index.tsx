6d1c275794e39c92566207a0c474c167e0182696
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-04-15 14:48:51 -0300

    Data Fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,29 @@ import type { NextPage } from 'next';
 import { Typography } from '@mui/material';
 
 import { ShopLayout } from '../components/layouts';
-import { initialData } from '../database/products';
 import { ProductsList } from '../components/products';
+import { useProducts } from '../hooks';
+import { FullScreenLoading } from '../components/ui';
 
-const Home: NextPage = () => {
+
+
+const HomePage: NextPage = () => {
+
+  const { products, isLoading } = useProducts('/products');
+
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos de Teslo aquí'}>
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
-      <ProductsList
-        products={initialData.products as any}
-      />
+      {
+        isLoading
+          ? <FullScreenLoading/>
+          : <ProductsList products={products} />
+      }
+
     </ShopLayout>
   )
 }
 
-export default Home
+export default HomePage
